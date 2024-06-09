033b4f809a3ed382776e013069cf80d43a35e343
TarasDutchak <Tarasdutchak90@gmail.com>
2024-06-09 08:29:35 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    const player = new Plyr('#player');
+
+    $('.date').inputmask({
+        "mask": "99 /99/ 9999",
+    });
+    $('.tel').inputmask({
+        "mask": "+ 380 99 999 9999",
+    });
+
+});

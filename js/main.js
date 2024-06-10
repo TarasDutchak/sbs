8bc1211c45953ca707a0e46a197ae96acb18c4b2
TarasDutchak <Tarasdutchak90@gmail.com>
2024-06-10 16:36:50 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -34,4 +34,25 @@ $(document).ready(function(){
         disable: 'mobile'
     });
 
+    var prevScrollpos = window.pageYOffset;
+
+    /* Get the header element and it's position */
+    var headerDiv = document.querySelector("header");
+    var headerBottom = headerDiv.offsetTop + headerDiv.offsetHeight;
+    
+    window.onscroll = function() {
+      var currentScrollPos = window.pageYOffset;
+    
+      /* if we're scrolling up, or we haven't passed the header,
+         show the header at the top */
+      if (prevScrollpos > currentScrollPos  || currentScrollPos < headerBottom){  
+          headerDiv.style.top = "0";
+      }
+      else{
+          /* otherwise we're scrolling down & have passed the header so hide it */
+          headerDiv.style.top = "-7.2rem";
+      } 
+    
+      prevScrollpos = currentScrollPos;
+    }
 });
